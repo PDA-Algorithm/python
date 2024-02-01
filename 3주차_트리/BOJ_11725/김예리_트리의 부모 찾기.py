5979446c4a50a4b20ca1de2aeb983ac99c41fbd0
yeri <rlafl7942@naver.com>
2024-02-01 18:38:04 +0900

    김예리/상근이의 여행.py

--- 3주차_트리/BOJ_11725/김예리_트리의 부모 찾기.py
+++ 3주차_트리/BOJ_11725/김예리_트리의 부모 찾기.py
@@ -1,6 +1,13 @@
 import sys
 
+
+input = sys.stdin.readline
+
+tree = {}
 n = int(input())
-for _ in range(n):
-  a, b = int(sys.stdin.readline().split())
-  
+for _ in range(n-1):
+  a, b = map(int, input().split())
+  tree[b]=a
+
+for i in range(n):
+  print(tree[i+1])
--- 3주차_트리/BOJ_9372/김예리_상근이의 여행.py
+++ 3주차_트리/BOJ_9372/김예리_상근이의 여행.py
@@ -2,10 +2,28 @@ import sys
 
 input = sys.stdin.readline
 
+def dfs (graph, v, visited, cnt):
+  visited[v] = True
+  for i in graph[v]:
+    if visited[i] == False:
+      cnt = dfs(graph, i, visited, cnt+1)
+  return cnt
+
+
 T = int(input())
 
 for _ in range(T):
   n, m = map(int, input().split())
+  visited = [False] * (n+1)
+  graph = [[] for _ in range(n+1)]
   for _ in range(m):
     a, b = map(int, input().split())
-  print(n-1)
+    graph[a].append(b)
+    graph[b].append(a)
+  cnt = dfs(graph, 1, visited, 0)
+  print(cnt)
+
+
+
+
+
