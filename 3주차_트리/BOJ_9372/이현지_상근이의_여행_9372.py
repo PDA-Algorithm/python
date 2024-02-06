0aec5588fd5c7b4a60b749fa7b34861311494cdb
Hyunji Lee <92327104+hleeat@users.noreply.github.com>
2024-02-06 16:46:20 +0900

    Create 이현지_상근이의_여행_9372.py

---
+++ 3주차_트리/BOJ_9372/이현지_상근이의_여행_9372.py
@@ -0,0 +1,9 @@
+#dfs
+import sys
+input=sys.stdin.readline
+test=int(input())
+for t in range(test):
+    n,m=map(int, input().split())
+    for _ in range(m):
+        u,v=map(int,input().split())
+    print(n-1)
